e3bf305d287c1efaf709072f3fb3d547ca2ae574
amanmountblue <aman.varyani@mountblue.tech>
2021-02-21 22:38:31 +0530

    plz work

---
+++ clear_db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+
+find . -path "*/migrations/*.pyc"  -delete
+
+rm db.sqlite3
+
+./manage.py makemigrations
+
+./manage.py migrate
